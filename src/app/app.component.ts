edbf62a89cdf5cbaec0df30fc315bdf3fc6c1a72
katharinahamburg <katharina@hinnrichs.de>
2023-07-26 21:15:27 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-todolist';
+  todos$: Observable<any>;
+  todos: Array<any>;
+
+  constructor(firestore: Firestore) {
+    const coll = collection(firestore, 'todos');
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe((newTodos) =>{
+      console.log('Neue Todos sind:', newTodos);
+      this.todos = newTodos;
+    });
+  }
 }
+
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'tsil-odot',
+    appId: '1:163263469299:web:f9762f7d054a52e63c8986',
+    storageBucket: 'tsil-odot.appspot.com',
+    apiKey: 'AIzaSyBU_w2Bc5d7KszYFXCVcOPNhsd-YrIi6nU',
+    authDomain: 'tsil-odot.firebaseapp.com',
+    messagingSenderId: '163263469299',
+    measurementId: 'G-0VZSKCMTQS',
+  },
+};
